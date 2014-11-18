4c54dcaf19a5dfa7e8abbcf48ae4841ec71465fe
quantumpotato <quantumpotato@gmail.com>
2014-11-17 23:59:25 -0500

    Extracted logic into .js

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 QuantumImprint #PROCJAM2014
 ==============
 
+Record game actions, play them back!
+
 Dynamic record/playback implementation of the cloning mechanics I used in Quantum Pliot [ https://itunes.apple.com/app/id935956154 ], inspired by Kenta Cho's DefeatMe. Use this library to record game events and play them back later, like when making clones that imitate a player.
+
+Sample implementation in imprint.html records the player's cursor position & color each frame. When the player clicks, the program begins playback, painting the same position & color where the player drew.
---
+++ imprint.js
@@ -0,0 +1,37 @@
+function createImprintable(key, info) {
+	var imprintHash = {"info" : info, "actions" : []};
+	imprintables[key] = imprintHash;
+}
+
+function createRecord(key, info, actions) {
+	info["key"] = key;
+	var recordHash = {"info" : info, "actions" : actions, "timeIndex": 0, "timeDirection" : 1};
+	records.push(recordHash);
+}
+
+function createRecordFromImprint(key) {
+		var imprintedRecord = imprintables[key];
+		createRecord(key, imprintedRecord["info"], imprintedRecord["actions"]);
+		imprintedRecord["actions"] = [];
+}
+
+function imprint(key, actions) {
+	var imprintHash = imprintables[key];
+	var imprintedActions = imprintHash["actions"];
+	imprintedActions.push(actions)
+}
+
+function playbackRecords() {
+	for (var i = 0; i < records.length; i++) {
+		var recordHash = records[i];
+		playbackRecordedImprint(recordHash["info"], recordHash["actions"][recordHash["timeIndex"]], recordHash["timeIndex"], recordHash["timeDirection"]);
+		recordHash["timeIndex"] = recordHash["timeIndex"] + recordHash["timeDirection"];
+		if (recordHash["timeIndex"] > recordHash["actions"].length - 1) {
+			recordHash["timeIndex"] = recordHash["actions"].length - 1;
+			recordHash["timeDirection"] = -1;
+		} else if (recordHash["timeIndex"] < 0) {
+			recordHash["timeIndex"] = 0
+			recordHash["timeDirection"] = 1;
+		}
+	}
+}
